b918689d2754ab422a8ab84c3d31d0156fb84010
sudha917 <sudha.avinash917.com>
2018-09-09 14:44:25 +0530

    MVC Application

---
+++ MyMVCApp/Controllers/MyfirstController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMVCApp.Models;
+
+namespace MyMVCApp.Controllers
+{
+    public class MyfirstController : Controller
+    {
+        // GET: Myfirst
+        public ActionResult Index()
+        {
+            Employee e = new Employee();
+            e.Id = 100;
+            e.Ename = "SBM";
+            ViewData["Date"] = DateTime.Now.ToString();
+            return View(e);
+        }
+        public ActionResult Index2()
+        {
+            
+            return View("MySecond");
+        }
+
+    }
+}
---
+++ MyMVCApp/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMVCApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+            public string Ename
+        {
+            get; set; }
+            public DateTime toady
+        {
+            get; set; }
+
+        }
+}
